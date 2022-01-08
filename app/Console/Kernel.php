ea32101104712d13df88c13b2702c4a87d59835f
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-01-08 10:08:22 -0500

    asd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('nullDay:daily')->timezone('America/Toronto')->between('9:52', '9:57');
         // $schedule->command('nullDay:daily')->everyMinute();
-        $schedule->command('nullDay:daily')->everyMinute();
+        // $schedule->command('nullDay:daily')->everyMinute();
         // $users = User::all();
 
 
