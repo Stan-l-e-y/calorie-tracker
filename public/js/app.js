ee0f54655cf69e5242119356fc6c184020c2d43b
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-01-08 11:00:29 -0500

    asd

--- public/js/app.js
+++ public/js/app.js
@@ -34230,7 +34230,7 @@ var render = function () {
           },
         },
         [
-          _c("label", { attrs: { for: "weight" } }, [_vm._v("Weight")]),
+          _c("label", { attrs: { for: "weight" } }, [_vm._v("sWeight")]),
           _vm._v(" "),
           _c("input", {
             directives: [
