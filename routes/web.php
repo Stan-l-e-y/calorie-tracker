b34ab8169d3c37dbfe4355112ab8ff469ba280a0
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-01-14 16:02:56 -0500

    Add FAQ Page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-})->middleware(['auth']);
+})->middleware(['auth'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
