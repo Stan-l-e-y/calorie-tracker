f207ef5f198a48a66bb4a1bae166bd7df2131969
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-01-14 18:10:48 -0500

    redo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-  <script src="{{ asset('js/app.js') }}" defer></script>
+  
     {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
