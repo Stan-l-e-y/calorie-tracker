8c6ff101f4aca5bfa839b0b19266ce53e0535228
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-01-14 18:15:30 -0500

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./resources/views/dashboard.blade.php",
+        "./Modules/**/*.blade.php",
     ],
 
     theme: {
