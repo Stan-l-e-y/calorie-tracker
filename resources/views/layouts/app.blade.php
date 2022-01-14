d8938b4a6b1da8d23c6aa2de381cfb20e1c8e9de
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-01-14 17:34:12 -0500

    asd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,7 +23,7 @@
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            {{-- <!-- Page Heading -->
+            <!-- Page Heading -->
             @if (request()->routeIs('dashboard'))
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -32,7 +32,7 @@
                     
                 </div>
             </header>
-            @endif --}}
+            @endif
 
             <!-- Page Content -->
             <main class="flex items-center justify-center flex-col  " >
